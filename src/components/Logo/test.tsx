5d0ebfe0f9e85361f4094946078c35962de70bc0
Eder R. Fleming <ederfleming@gmail.com>
2021-05-09 19:13:42 -0300

    Add renderWithTheme helper

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    //1 - renderizar o componente com o 'render'
+    //2- selecionar o elemento a ser testacom com o 'screen' + (queries) - getByLabel...
+    //3- analisar o comportamento esperado com o 'expect'
+  })
+})
