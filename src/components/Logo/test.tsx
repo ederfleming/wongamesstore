2c0ccdf819784b9a47c458057292840fccabd693
Eder R. Fleming <ederfleming@gmail.com>
2021-05-11 14:39:56 -0300

    Add menu mobile and desktop with tests

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,6 +1,5 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -35,8 +34,10 @@ describe('<Logo />', () => {
 
   it('should render a bigger logo without text if hideOnMobile', () => {
     renderWithTheme(<Logo hideOnMobile />)
-    expect(
-      screen.getByLabelText(/Won Games/i).parentElement
-    ).toHaveStyleRule('width', '5.8rem', { media: '(max-width: 768px)' })
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width: 768px)' }
+    )
   })
 })
