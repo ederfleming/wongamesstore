28a2827c855dc810d9938057efccf2339d2949df
Eder R. Fleming <ederfleming@gmail.com>
2021-05-12 12:18:59 -0300

    Add Button to support anchor/button types

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,69 @@
+import Link from 'next/link'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact
+        </Heading>
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+        <a href="tel:+551199999-9999">+551199999-9999</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Follow us
+        </Heading>
+        <nav arua-aria-labelledby="social media">
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Instagram
+          </a>
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Twitter
+          </a>
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            YouTube
+          </a>
+          <a href="#" target="_blank" rel="noopenner, noreferrer">
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Links
+        </Heading>
+        <nav arua-aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/">
+            <a>Store</a>
+          </Link>
+          <Link href="/">
+            <a>Search</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Location
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem ipsum</span>
+        <span>Lorem ipsum - dolor sit.</span>
+      </S.Column>
+    </S.Content>
+    <S.Copyright>Won Games 2021 @ All rights reserved.</S.Copyright>
+  </S.Wrapper>
+)
+
+export default Footer
