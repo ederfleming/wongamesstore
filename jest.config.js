2a0eb18d2350f7371fedc1e823ffc3577870d906
Eder R. Fleming <ederfleming@gmail.com>
2021-05-22 12:31:49 -0300

    Add show case and home refactor

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,7 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
-    '!src/utils/apollo.ts',
-    '!src/utils/apolloCache.ts',
     '!src/types/**/*.d.ts',
-    '!src/graphql/**/*.ts',
     '!src/**/mock.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
---
+++ src/components/Showcase/stories.tsx
@@ -0,0 +1,49 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Showcase, { ShowcaseProps } from '.'
+
+import highlightMock from 'components/Highlight/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
+export default {
+  title: 'Showcase',
+  component: Showcase,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '0 auto' }}>
+        <Story />
+      </div>
+    )
+  ],
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<ShowcaseProps> = (args) => <Showcase {...args} />
+
+Default.args = {
+  title: 'Most populars',
+  highlight: highlightMock,
+  games: gamesMock
+}
+
+export const WithoutHighlight: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+WithoutHighlight.args = {
+  title: 'Most populars',
+  games: gamesMock
+}
+
+export const WithoutGames: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+)
+
+WithoutGames.args = {
+  title: 'Most populars',
+  highlight: highlightMock
+}
