1704bab9dd9d339368843ef7dcebd45281396b67
Eder R. Fleming <ederfleming@gmail.com>
2021-05-24 12:20:59 -0300

    Add profile pages

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,11 @@
+//import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Profile from '.'
+
+describe('<Profile />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Profile />)
+  })
+})
