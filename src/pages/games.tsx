5c8e672d3b136b023b805070e6bd0acb26149ef4
Eder R. Fleming <ederfleming@gmail.com>
2021-05-31 10:22:28 -0300

    Populando pagina de games dinamicamente

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,33 @@
+import { QUERY_GAMES } from 'graphql/queries/games'
+
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 
+import { initializeApollo } from 'utils/apollo'
+
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
+  })
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(game.price)
+      })),
       filterItems: filterItemsMock
     }
   }
