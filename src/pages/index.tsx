85340a6a7b977a2c60046127d355df8409db3d9b
Eder R. Fleming <ederfleming@gmail.com>
2021-06-02 12:22:40 -0300

    Add dinamic banners in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import bannersMock from 'components/BannerSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
@@ -12,11 +16,25 @@ export default function Index(props: HomeTemplateProps) {
 // getStaticProps => gerar estático em build time (gatsby)
 // getServerSideProps => gerar via ssr a cada request (nunca vai para o bundle do client)
 // getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do 1 req)
-export async function getServerSideProps() {
-  // retorno dos dados
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 10,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
