0d6ef6c84f4bc39eb2070b35b8ab4bf51c7f7714
Eder R. Fleming <ederfleming@gmail.com>
2021-06-12 22:35:02 -0300

    Fix games template tests e create apolloCache.ts

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,13 +1,15 @@
 import { screen } from '@testing-library/react'
 
 import { MockedProvider } from '@apollo/client/testing'
-import { QUERY_GAMES } from 'graphql/queries/games'
+import userEvent from '@testing-library/user-event'
 
+import apolloCache from 'utils/apolloCache'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
 
 import Games from '.'
+import { fetchMoreMock, gamesMock } from './mock'
 
 jest.mock('templates/Base', () => ({
   __esModule: true,
@@ -21,7 +23,6 @@ jest.mock('components/ExploreSidebar', () => ({
     return <div data-testid="Mock ExploreSidebar">{children}</div>
   }
 }))
-
 describe('<Games />', () => {
   it('should render loading when starting the template', () => {
     renderWithTheme(
@@ -29,56 +30,41 @@ describe('<Games />', () => {
         <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
-
     expect(screen.getByText(/loading.../i)).toBeInTheDocument()
   })
 
   it('should render sections', async () => {
     renderWithTheme(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: QUERY_GAMES,
-              variables: { limit: 15 }
-            },
-            result: {
-              data: {
-                games: [
-                  {
-                    name: 'RimWorld',
-                    slug: 'rimworld',
-                    cover: {
-                      url: '/uploads/rimworld_8e93acc963.jpg'
-                    },
-                    developers: [{ name: 'Ludeon Studios' }],
-                    price: 65.99,
-                    __typename: 'Game'
-                  }
-                ]
-              }
-            }
-          }
-        ]}
-        addTypename={false}
-      >
+      <MockedProvider mocks={[gamesMock]} addTypename={false}>
         <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
-
     // it starts without data
     // shows loading
     expect(screen.getByText(/loading.../i)).toBeInTheDocument()
-
     // we wait until we have data to get the elements
     // get => tem certeza do elemento
     // query => Não tem o elemento
     // find => processos assincronos
     expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
-    expect(await screen.findByText(/RimWorld/i)).toBeInTheDocument()
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
     expect(
       await screen.findByRole('button', { name: /show more/i })
     ).toBeInTheDocument()
+  })
+
+  it('should render more games when show more is clicked', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
+
+    userEvent.click(await screen.findByRole('button', { name: /show more/i }))
+
+    expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
   })
 })
--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,12 +1,8 @@
 import { useMemo } from 'react'
 
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  NormalizedCacheObject
-} from '@apollo/client'
-import { concatPagination } from '@apollo/client/utilities'
+import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client'
+
+import apolloCache from './apolloCache'
 
 let apolloClient: ApolloClient<NormalizedCacheObject>
 
@@ -14,36 +10,24 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
-    cache: new InMemoryCache({
-      typePolicies: {
-        Query: {
-          fields: {
-            games: concatPagination()
-          }
-        }
-      }
-    })
+    cache: apolloCache
   })
 }
 
 export function initializeApollo(initialState = {}) {
   // serve para verificar se já existe uma instância, para não criar outra
   const apolloClientGlobal = apolloClient ?? createApolloClient()
-
   // se a página usar o apolloClient no lado client
   // hidratamos o estado inicial aqui
   if (initialState) {
     apolloClientGlobal.cache.restore(initialState)
   }
-
   // sempre inicializando no SSR com cache limpo
   if (typeof window === 'undefined') return apolloClientGlobal
   // cria o apolloClient se estiver no client side
   apolloClient = apolloClient ?? apolloClientGlobal
-
   return apolloClient
 }
-
 export function useApollo(initialState = {}) {
   const store = useMemo(() => initializeApollo(initialState), [initialState])
   return store
