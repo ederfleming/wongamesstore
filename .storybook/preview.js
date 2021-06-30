fcf5cc243a9f3e2d258c8def6bdb6a8056b9c366
Eder R. Fleming <ederfleming@gmail.com>
2021-06-30 11:04:48 -0300

    Configure Storybook to use ContextProvider and use args

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,22 @@
+import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const decorators = [
-  (Story) => (
+  (Story, context) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles removeBg />
-      <Story />
+      <CartContext.Provider
+        value={{
+          ...CartContextDefaultValues,
+          ...(context?.args?.CartContextValue || {}),
+          ...context.args
+        }}
+      >
+        <GlobalStyles removeBg />
+        <Story />
+      </CartContext.Provider>
     </ThemeProvider>
   )
 ]
--- src/components/CartIcon/stories.tsx
+++ src/components/CartIcon/stories.tsx
@@ -1,6 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 
-import CartIcon, { CartIconProps } from '.'
+import CartIcon from '.'
 
 export default {
   title: 'CartIcon',
@@ -13,8 +13,8 @@ export default {
 } as Meta
 
 export const Default: Story = () => <CartIcon />
-export const WithItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
+export const WithItems: Story = (args) => <CartIcon {...args} />
 
 WithItems.args = {
-  quantity: 12
+  quantity: 3
 }
