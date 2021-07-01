856485ec263c1e9282f11550525ae5668634533c
Eder R. Fleming <ederfleming@gmail.com>
2021-07-01 20:01:20 -0300

    Add context in CartDropdown

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,3 +1,5 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
+
 import { render, screen } from 'utils/test-utils'
 
 import items from 'components/CartList/mock'
@@ -5,19 +7,23 @@ import items from 'components/CartList/mock'
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 300,00'
+    }
+
+    render(<CartDropdown />, { cartProviderProps })
+  })
   it('should render <CartIcon/> and its badge', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />)
-
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
-
-    //expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
+    expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />)
-
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
-    expect(screen.getByText('Borderlands 3')).toBeInTheDocument()
-    //expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
+    expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
 })
