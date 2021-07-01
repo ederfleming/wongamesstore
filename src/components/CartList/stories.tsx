4ef2522bab1175a145bf6e0a43ec1e5c11175af2
Eder R. Fleming <ederfleming@gmail.com>
2021-07-01 19:42:48 -0300

    Add context in CartList

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,35 +1,49 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 
-import CartList, { CartListProps } from '.'
-import mockItems from './mock'
+import CartList from '.'
+import items from './mock'
 
 export default {
   title: 'Profile/CartList',
   component: CartList,
-  args: {
-    items: mockItems,
-    total: 'R$ 330,00'
-  },
-  parameters: {
-    backgrounds: {
-      default: 'won-dark'
+  argTypes: {
+    cartContextValue: {
+      type: ''
+    },
+    items: {
+      type: ''
+    },
+    parameters: {
+      backgrounds: {
+        default: 'won-dark'
+      }
     }
   }
 } as Meta
 
-export const Default: Story<CartListProps> = (args) => (
+export const Default: Story = (args) => (
   <div style={{ maxWidth: 800 }}>
     <CartList {...args} />
   </div>
 )
 
-export const WithButton: Story<CartListProps> = (args) => (
+Default.args = {
+  total: 'R$330,00',
+  cartContextValue: { items }
+}
+
+export const WithButton: Story = (args) => (
   <div style={{ maxWidth: 800 }}>
     <CartList {...args} hasButton />
   </div>
 )
 
-export const Empty: Story<CartListProps> = () => (
+WithButton.args = {
+  total: 'R$330,00',
+  cartContextValue: { items }
+}
+
+export const Empty: Story = () => (
   <div style={{ maxWidth: 800 }}>
     <CartList />
   </div>
