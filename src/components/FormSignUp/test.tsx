8d8a86bce16ff648bdbdc4483dfa9caf2b281b32
Eder R. Fleming <ederfleming@gmail.com>
2021-07-12 16:28:02 -0300

    Fix FormSignUp tests

--- src/components/FormSignUp/test.tsx
+++ src/components/FormSignUp/test.tsx
@@ -1,12 +1,18 @@
+import { MockedProvider } from '@apollo/client/testing'
+
 import { render, screen } from 'utils/test-utils'
 
 import FormSignUp from '.'
 
 describe('<FormSignUp />', () => {
   it('should render the heading', () => {
-    render(<FormSignUp />)
+    render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>
+    )
 
-    expect(screen.getByPlaceholderText('Name')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Username')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Confirm Password')).toBeInTheDocument()
@@ -16,7 +22,11 @@ describe('<FormSignUp />', () => {
   })
 
   it('should render the text to sign up', () => {
-    render(<FormSignUp />)
+    render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>
+    )
 
     expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /sign in/i })).toBeInTheDocument()
